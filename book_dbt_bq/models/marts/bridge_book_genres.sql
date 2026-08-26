3253585211809daafcb8dc10d4f0cd0cd802b322
Panda <roxanaelenagenes@gmail.com>
2026-08-26 13:03:54 +0300

    Add dashboard exports and pbix file

--- book_dbt_bq/models/marts/bridge_book_genres.sql
+++ book_dbt_bq/models/marts/bridge_book_genres.sql
@@ -3,4 +3,6 @@ select distinct
     trim(genre_name) as genre_name
 from {{ ref('int_books_cleaned') }},
     unnest(split(genres, ',')) as genre_name
-where trim(genre_name) != ''
+where trim(genre_name) in (
+    select genre_name from {{ ref('dim_genres') }}
+)
